c9e3b9bac75f0ffa9d890c91f7be53ea9977eaf1
tomrr6 <tomrr6@hotmail.com>
2022-04-29 18:04:26 -0500

    js vh works now

--- js/localStorage.js
+++ js/localStorage.js
@@ -106,9 +106,9 @@ function updateSettings(key, value) {
 if (getSettings() == null) {
     resetSettings();
 }
-if (getleaderboard() == null) {
-    resetLeaderboard();
-}
+// if (getleaderboard() == null) {
+//     resetLeaderboard();
+// }
 
 
 // createNewCurrentGame();
@@ -122,3 +122,5 @@ console.log(getCurrentGame());
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--fullvh', `${window.innerHeight}px`);
+console.log(`${window.innerHeight}px`);
